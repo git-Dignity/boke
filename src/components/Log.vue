bbcf295211625d01bf636570f8ad71059164e67a
zzm <1638153584@qq.com>
2019-12-06 16:50:53 +0800

    拉取test1

--- src/components/Log.vue
+++ src/components/Log.vue
@@ -1,43 +1,35 @@
 <template>
-    <div>
-       <BlogHeader></BlogHeader>
-      
-       <div style="margin-top: 20px">
-        <el-radio v-model="radio2" label="1" border size="medium">登录日志查询</el-radio>
-        <el-radio v-model="radio2" label="2" border size="medium">操作日志查询</el-radio>
-      </div>
-      <br /><br />
+  <div>
+    <BlogHeader></BlogHeader>
 
+    <div style="margin-top: 20px">
+      <el-radio v-model="radio2" label="1" border size="medium">登录日志查询</el-radio>
+      <el-radio v-model="radio2" label="2" border size="medium">操作日志查询</el-radio>
     </div>
+    <br />
+    <br />
+  </div>
 </template>
 
 <style>
 </style>
 <script>
-  import jquery from '../../static/jquery-1.9.1.min'
-  import BlogHeader from './BlogHeader.vue'
-    
-  export default {
-     components:{
-       BlogHeader
-    },
-    name: 'log',
-    data () {
-      return {
-        radio2: '1',
-      
-      }
-    },
-     created(){
+import jquery from "../../static/jquery-1.9.1.min";
+import BlogHeader from "./BlogHeader.vue";
 
-     },
-      mounted(){
-       
-      },
-  }
+export default {
+  components: {
+    BlogHeader
+  },
+  name: "log",
+  data() {
+    return {
+      radio2: "1"
+    };
+  },
+  created() {}
+};
 </script>
 
 <style scoped>
-
-
 </style>
